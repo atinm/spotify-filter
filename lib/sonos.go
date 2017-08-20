afab5d9f8db11949a07bdb3997ade53fd444854a
Atin M <atinm.dev@gmail.com>
2017-08-20 18:50:51 -0400

    Add ability to find radio-friendly songs and skip to them

--- lib/sonos.go
+++ lib/sonos.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/atinm/go-sonos"
@@ -133,6 +134,8 @@ func handleAVTransportEvents(reactor upnp.Reactor, c chan bool) {
 				uri := b.LastChange.InstanceID.CurrentTrackURI.Val
 				log.Printf("[DEBUG] CurrentTrackURI: %v", b.LastChange.InstanceID.CurrentTrackURI.Val)
 				log.Printf("[DEBUG] CurrentTrackMetadata: %s", b.LastChange.InstanceID.CurrentTrackMetaData.Val)
+				log.Printf("[DEBUG] CurrentTrack: %s", b.LastChange.InstanceID.CurrentTrack.Val)
+				currentTrack, _ := strconv.Atoi(b.LastChange.InstanceID.CurrentTrack.Val)
 				if strings.HasPrefix(uri, "x-sonos-spotify:spotify%3atrack%3a") {
 					log.Print("Playing Spotify track")
 					if b.LastChange.InstanceID.CurrentTrackMetaData.Val != "" {
@@ -140,18 +143,26 @@ func handleAVTransportEvents(reactor upnp.Reactor, c chan bool) {
 						err := xml.Unmarshal([]byte(b.LastChange.InstanceID.CurrentTrackMetaData.Val), &doc)
 						if err != nil {
 							log.Printf("[ERROR] Could not unmarshal %s: %v", b.LastChange.InstanceID.CurrentTrackMetaData.Val, err)
+							continue
 						}
 						for _, item := range doc.Item {
-							artist = item.Creator[0].Value
-							album = item.Album[0].Value
-							title = item.Title[0].Value
+							for _, a := range item.Creator {
+								artist = a.Value
+							}
+							for _, l := range item.Album {
+								album = l.Value
+							}
+							for _, t := range item.Title {
+								title = t.Value
+							}
 							log.Printf("[DEBUG] title: %s, artist: %s, album: %s", title, artist, album)
 							break
 						}
 					}
 
 					// extract the trackid from the uri
-					trackID := strings.Split(strings.TrimPrefix(uri, "x-sonos-spotify:spotify%3atrack%3a"), "?")[0]
+					parsedTrackURI := strings.Split(strings.TrimPrefix(uri, "x-sonos-spotify:spotify%3atrack%3a"), "?")
+					trackID := parsedTrackURI[0]
 					track, err := client.GetTrack(spotify.ID(trackID))
 					if err != nil {
 						log.Printf("[WARN] Could not get track info for trackId: (%s): %v\n", trackID, err)
@@ -171,17 +182,71 @@ func handleAVTransportEvents(reactor upnp.Reactor, c chan bool) {
 						log.Printf("[DEBUG] Position.TrackURI: %s", posInfo.TrackURI)
 						log.Printf("[DEBUG] Position.TrackDuration: %s", posInfo.TrackDuration)
 						log.Printf("[DEBUG] Position.RelTime: %s", posInfo.RelTime)
+						var query string
+						query = fmt.Sprintf("title:%s artist:%s album:%s", title, artist, album)
+						log.Printf("[DEBUG] Querying spotify for %s", query)
+						// search for radio friendly track for artist, title, album, explicit=false instead
+						if res, err := client.Search(query, spotify.SearchTypeTrack); err != nil || len(res.Tracks.Tracks) == 0 {
+							log.Printf("Could not find '%s' by '%s' in search: %v", title, artist, err)
+							player.Next(0)
+							if err != nil {
+								log.Printf("[WARN] Could not skip track '%s' by '%s': %v", title, artist, err)
+								continue
+							}
+							log.Printf("[INFO] Skipped '%s' by '%s' playing on '%s'\n", title, artist, player.Player.RoomName)
+						} else {
+							var complete = false
+							for _, track := range res.Tracks.Tracks {
+								log.Print("Found tracks searching for alternates")
+								if !track.Explicit {
+									log.Print("Found radio-friendly track")
+									uri := fmt.Sprintf("x-sonos-spotify:spotify%%3atrack%%3a%s?%s", track.ID, parsedTrackURI[1])
+									//var uri = "x-sonos-spotify:spotify%3atrack%3a" + trackID
+									var meta = "<DIDL-Lite xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\" xmlns:r=\"urn:schemas-rinconnetworks-com:metadata-1-0/\" xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\"><item id=\"-1\" parentID=\"-1\" restricted=\"true\"><res protocolInfo=\"sonos.com-spotify:*:audio/x-spotify:*\" duration=\"" + posInfo.TrackDuration + "\">x-sonos-spotify:spotify%3atrack%3a" + trackID + "?sid=12&amp;flags=8224&amp;sn=8</res><r:streamContent></r:streamContent><r:radioShowMd></r:radioShowMd><upnp:albumArtURI>/getaa?s=1&amp;u=x-sonos-spotify%3aspotify%253atrack%253a" + trackID + "%3fsid%3d12%26flags%3d8224%26sn%3d8</upnp:albumArtURI><dc:title>" + title + "</dc:title><upnp:class>object.item.audioItem.musicTrack</upnp:class><dc:creator>" + artist +
+										"</dc:creator><upnp:album>" + album + "</upnp:album><r:tags>1</r:tags></item></DIDL-Lite>"
+									req := upnp.AddURIToQueueIn{
+										EnqueuedURI:                     uri,
+										EnqueuedURIMetaData:             meta,
+										DesiredFirstTrackNumberEnqueued: uint32(currentTrack + 1),
+									}
+									log.Printf("Queued: %s", meta)
+									if _, err := player.AddURIToQueue(0 /*instanceId*/, &req); nil != err {
+										log.Printf("[INFO] Couldn't add radio-friendly version '%s', '%s' of '%s' by '%s', skipped playing on '%s': %v\n",
+											track.ID, track.URI, title, artist, player.Player.RoomName, err)
+										err = player.Next(0)
+										if err != nil {
+											log.Printf("[WARN] Could not skip track '%s' by '%s': %v", title, artist, err)
+											break
+										}
+										complete = true
+									} else {
+										err = player.Next(0)
+										if err != nil {
+											log.Printf("[WARN] Could not skip track '%s' by '%s': %v", title, artist, err)
+											break
+										}
+										log.Printf("[INFO] Skipped to radio-friendly version '%s' of '%s' by '%s' playing on '%s'", uri, title, artist, player.Player.RoomName)
+										complete = true
+									}
+									break
+								}
+							}
 
-						err = player.Next(0)
-						if err != nil {
-							log.Printf("[WARN] Could not skip track '%s' by '%s': %v", title, artist, err)
-							continue
+							if !complete {
+								err = player.Next(0)
+								if err != nil {
+									log.Printf("[WARN] Could not skip track '%s' by '%s': %v", title, artist, err)
+									continue
+								}
+								log.Printf("[INFO] Skipped '%s' by '%s' playing on '%s'\n", title, artist, player.Player.RoomName)
+							}
 						}
-						log.Printf("[INFO] Skipped '%s' by '%s' playing on '%s'\n", title, artist, player.Player.RoomName)
 					}
 				} else {
 					log.Printf("Not playing Spotify track: %s", uri)
 				}
+			case upnp.ContentDirectory_EventType:
+
 			default:
 				log.Panicf("[ERROR] Unexpected event %#v", evt)
 			}
@@ -193,7 +258,7 @@ func SetupEvents(mgr ssdp.Manager) {
 	// Startup and listen to events
 	reactor := sonos.MakeReactor(port)
 	go handleAVTransportEvents(reactor, exit_chan)
-	sonosDevices = sonos.ConnectAll(mgr, reactor, sonos.SVC_AV_TRANSPORT)
+	sonosDevices = sonos.ConnectAll(mgr, reactor, sonos.SVC_AV_TRANSPORT|sonos.SVC_CONTENT_DIRECTORY)
 	log.Printf("[DEBUG] Set up event handler for Sonos, found %d devices", len(sonosDevices))
 	for _, player := range sonosDevices {
 		log.Printf("Found %s", player.RoomName)
